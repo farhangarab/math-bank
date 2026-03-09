5fa3d00fc698c6e5aff252bc74244f96cd2108fa
Farhang Arab Zada <farhangarab123@gmail.com>
2026-03-09 16:20:32 -0400

    add database configuration and User model

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from app import create_app, db
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route("/")
-def home():
-    return {"message": "MathBank API Running"}
-
+with app.app_context():
+    db.create_all()
+    
 if __name__ == "__main__":
     app.run(debug=True)
