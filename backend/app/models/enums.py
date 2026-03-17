6ec54c198366a3a5afe363987699f2351c2716a3
Farhang Arab Zada <farhangarab123@gmail.com>
2026-03-16 21:12:13 -0400

    add UserRole enum, error handling and validation

---
+++ backend/app/models/enums.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class UserRole(enum.Enum):
+    STUDENT = "STUDENT"
+    TEACHER = "TEACHER"
--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, request, jsonify
 from app import db
 from app.models.user import User
+from app.models.enums import UserRole
 import bcrypt
 
 auth_bp = Blueprint("auth", __name__)
+
+# Mock code
+TEACHER_CODE = "ABC123"
 
 
 @auth_bp.route("/register", methods=["POST"])
@@ -12,22 +16,45 @@ def register():
     data = request.json
 
     username = data.get("username")
+    full_name = data.get("full_name")
     email = data.get("email")
     password = data.get("password")
     role = data.get("role")
+    teacher_code = data.get("teacher_code")
 
-    if not username or not password:
+    # check all field exist
+    if not username or not password or not email or not role:
         return jsonify({"error": "Missing fields"}), 400
 
-    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    # check the role
+    if not role in ["STUDENT", "TEACHER"]:
+        return jsonify({"error": "Invalid role"}), 400
+
+    # check teacher code
+    if role == "TEACHER":
+        if teacher_code != TEACHER_CODE:
+            return jsonify({"error": "Invalid teacher code"}), 400
+
+    # check unique username & email
+    if User.query.filter_by(username=username).first():
+        return jsonify({"error": "Username exists"}), 400
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({"error": "Email exists"}), 400
+
+    # hash the password
+    hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+    # create the user
     user = User(
         username=username,
+        full_name=full_name,
         email=email,
         password_hash=hashed.decode(),
-        role=role,
+        role=UserRole(role),
     )
 
     db.session.add(user)
     db.session.commit()
 
-    return jsonify({"message": "User created"})
+    return jsonify({"message": "User created"}), 201
