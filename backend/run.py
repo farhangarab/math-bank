6ea75209b36843c34b9b705580cb174b2be8f5e5
Farhang Arab Zada <farhangarab123@gmail.com>
2026-05-09 16:56:02 -0400

    Initialize database on startup for free deployment

--- backend/run.py
+++ backend/run.py
@@ -2,14 +2,16 @@ from app import create_app, db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 
 @app.cli.command("init-db")
 def init_db():
     db.create_all()
     print("Database tables created.")
-    
+
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
 
     app.run(debug=app.config["FLASK_DEBUG"])
